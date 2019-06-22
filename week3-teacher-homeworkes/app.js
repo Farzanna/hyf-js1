d53eb161defd3075cc1cdae64d4a20017c4bf94a
Farzanna F <nickbenzanna@gmail.com>
2019-06-23 01:50:48 +0200

    Update app.js

--- week3-teacher-homeworkes/app.js
+++ week3-teacher-homeworkes/app.js
@@ -1,4 +1,4 @@
-//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-question1:Array manipulation +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-question1:Array manipulation +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 
 //Array manipulation
 //Arguement: [5, 3, 2, 9, 19, 20, 3, 5] First, remove both 3 s from the array, then add a 100 to the array.
@@ -31,53 +31,85 @@ console.log(editArray(myArray, 3, 100));
 
 
 
-//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-question2: Object Fun +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-question2: Object Fun +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 
 // Object Fun
-// Make an array of pet objects, this pet object should include animal name animal weight animalSound - the animal sound should be a function that console logs the sound the animal makes (meow, moo, oink) loop through the array and print out the animal sound and name
+// Make an array of pet objects, this pet object should include animal name animal weight animalSound - the animal sound
+//should be a function that console logs the sound the animal makes (meow, moo, oink) loop through the array and print out
+//the animal sound and name
 // Return: void (nothing)
 
-let petsName = ["cat", "dog", "bird", "sheep", "hen", "rooster", "cow"];
-let petsWeight = ["5kg", "8kg", "0.5kg", "15kg", "2kg", "2kg", "30kg"];
-let petsSound = ["mew", "woof", "tweet", "baa", "kokoak", "kykyrky", "moo"];
+// Object Fun
+// Make an array of pet objects, this pet object should include animal name animal weight animalSound - the animal sound 
+//should be a function that console logs the sound the animal makes (meow, moo, oink) loop through the array and print out 
+//the animal sound and name
+// Return: void (nothing)
+let petsObject = [
+  {
+    name: 'cat',
+    weight: '5kg',
+    getSound: () => {
+      console.log(' mew');
+    },
+  },
+  {
+    name: 'dog',
+    weight: '8kg',
+    getSound: () => {
+      console.log(' woof');
+    },
+  },
+  {
+    name: 'bird',
+    weight: '0.3kg',
+    getSound: () => {
+      console.log('tweet');
+    },
+  },
+];
 
-function getPetInfo(petsNameX, petsSoundX, petsWeightX) {
-  for (i = 0; i < petsName.length; i++) {
-    console.log(petsNameX[i] +
-      ': ' +
-      petsSoundX[i] +
-      ' ,' +
-      petsWeightX[i]);
-  }
-}
-getPetInfo(petsName, petsSound, petsWeight);
+petsObject.forEach(element => {
+  console.log(element.name + ': ');
+  element.getSound();
+});
 
 
 
 
-//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-question3: String Manipulation +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-question3: String Manipulation +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 
 // String Manipulation
-// Arguement: 'Hello, it is a good day, I eat good bananas, I do not like the word good' remove the word 'good' from the string
+// Arguement: 'Hello, it is a good day, I eat good bananas, I do not like the word good' remove the word 'good' from the
+//string
 // Return: String
+// Write a program that prints out, in reverse order, every multiple of 3 between 1 and 200. output should be 198 195 192 ...
+// Return: void (nothing)
+let minNum = 1;
+let maxNum = 200;
+let multipleNum = 3;
 
-let myString = 'Hello, it is a good day, I eat good bananas, I do not like the word good';
-let oldChar = /good/gi;
-let newChar = "";
-
-function replaceChar(myStringX, oldCharX, newCharX) {
-  let newMyString = myStringX.replace(oldCharX, newCharX);
-  return newMyString;
+function findMultipleNumbs1(minNumX, maxNumX, multipleNumX) {
+  if (maxNumX > minNumX && maxNumX >= multipleNumX) {
+    if (maxNumX % multipleNumX == 0) {
+      console.log(maxNumX);
+      maxNumX--;
+      findMultipleNumbs1(minNumX, maxNumX, multipleNumX);
+    } else if (maxNumX > minNumX) {
+      maxNumX--;
+      findMultipleNumbs1(minNumX, maxNumX, multipleNumX);
+    }
+  }
 }
-console.log(replaceChar(myString, oldChar, newChar));
+findMultipleNumbs1(minNum, maxNum, multipleNum);
+
+return;
 
 
 
 
-//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-question4: Conditionals +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-
+//+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-question4: Conditionals +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+
 
-// Write a program that prints out, in reverse order, every multiple of 3 between 1 and 200. output should be 198 195 192 ......
-
+// Write a program that prints out, in reverse order, every multiple of 3 between 1 and 200. output should be 198 195 192 ...
 // Return: void (nothing)
 
 let endNum = 200;
